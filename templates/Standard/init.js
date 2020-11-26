93d69c66c595e13caaed4bc5dc96d8d136ad162f
Martin Collins <martin@hatchlane.com>
2020-11-26 12:36:06 +0000

    Fix #19: Abbreviate long template store names
    There are 79 weapons who's names exceed the 27 character limit of the field.
    
    Rather than word wrap the store names, we slightly rename them to fit so the
    pilot isn't left guessing which store to use

--- templates/Standard/init.js
+++ templates/Standard/init.js
@@ -391,7 +391,91 @@ var Loadout = function(data, unit) {
 
   var data = data;
   var unit = unit;
-  
+
+  // This template has 27 character limit on store name, so these are required
+  // to reamp long names to more kneeboard friendly format.
+
+  var clsid_renames = {
+    "LAU3_HE151": "LAU-3 - 19 MK151 HE",
+    "LAU3_HE5": "LAU-3 - 19 MK5 HEAT",
+    "LAU3_WP156": "LAU-3 - 19 MK156 WP",
+    "LAU3_WP1B": "LAU-3 - 19 WTU-1/B WP",
+    "LAU3_WP61": "LAU-3 - 19 MK61 WP",
+    "{BRU33_LAU10}": "BRU-33 LAU-10 - 4 ZUNI MK71",
+    "{BRU33_LAU61}": "BRU-33 LAU-61 - 19 M151",
+    "{BRU33_LAU68}": "BRU-33 LAU-68 - 7 M151",
+    "{BRU33_LAU68_MK5}": "BRU-33 LAU-68 - 7 MK5",
+    "{BRU33_2*LAU10}": "BRU-33 - 2 LAU-10 - 4 ZUNI",
+    "{BRU33_2*LAU61}": "BRU-33 - 2 LAU-61 - 19 M151",
+    "{BRU33_2*LAU68}": "BRU-33 - 2 LAU-68 - 7 M151",
+    "{BRU33_2*LAU68_MK5}": "BRU-33 - 2 LAU-68 - 7 MK5",
+    "LAU-115_2*LAU-127_AIM-9L": "LAU-115 - 2 LAU-127 AIM-9L",
+    "LAU-115_2*LAU-127_AIM-9M": "LAU-115 - 2 LAU-127 AIM-9M",
+    "LAU-115_2*LAU-127_AIM-9X": "LAU-115 - 2 LAU-127 AIM-9X",
+    "LAU-115_LAU-127_AIM-9L": "LAU-115C LAU-127 AIM-9L",
+    "LAU-115_LAU-127_AIM-9M": "LAU-115C LAU-127 AIM-9M",
+    "LAU-115_LAU-127_AIM-9X": "LAU-115C LAU-127 AIM-9X",
+    "{FPU_8A_FUEL_TANK}": "FPU-8A Tank 330 gal",
+    "LAU-105_1*AIM-9L_L": "LAU-105 AIM-9L",
+    "LAU-105_1*AIM-9M_L": "LAU-105 AIM-9M",
+    "LAU-105_2*AIM-9L": "LAU-105 - 2 AIM-9L",
+    "{DB434044-F5D0-4F1F-9BA9-B73027E18DD3}": "LAU-105 - 2 AIM-9M",
+    "{69926055-0DA8-4530-9F2F-C86B157EA9F6}": "LAU-131 - 7 M151 (HE)",
+    "{2AF2EC3F-9065-4de5-93E1-1739C9A71EF7}": "LAU-131 - 7 M156 (WP)",
+    "{DAD45FE5-CFF0-4a2b-99D4-5D044D3BC22F}": "LAU-131 - 7 M257",
+    "{6D6D5C07-2A90-4a68-9A74-C5D0CFFB05D9}": "LAU-131 - 7 M274",
+    "{319293F2-392C-4617-8315-7C88C22AF7C4}": "LAU-131 - 7 MK5 (HE)",
+    "{1CA5E00B-D545-4ff9-9B53-5970E292F14D}": "LAU-131 - 7 MK61 (Practice)",
+    "{D22C2D63-E5C9-4247-94FB-5E8F3DE22B71}": "LAU-131 - 7 Mk1 (Practice)",
+    "{DDCE7D70-5313-4181-8977-F11018681662}": "LAU-131 - 7 WTU1B",
+    "{LAU-131 - 7 AGR-20A}": "LAU-131 - 7 M151 HE APKWS",
+    "{LAU-131 - 7 AGR-20 M282}": "LAU-131 M282 - MPP APKWS",
+    "{A021F29D-18AB-4d3e-985C-FC9C60E35E9E}": "LAU-68 - 7 M151 (HE)",
+    "{4F977A2A-CD25-44df-90EF-164BFA2AE72F}": "LAU-68 - 7 M156(WP)",
+    "{647C5F26-BDD1-41e6-A371-8DE1E4CC0E94}": "LAU-68 - 7 M257",
+    "{0877B74B-5A00-4e61-BA8A-A56450BA9E27}": "LAU-68 - 7 M274",
+    "{FC85D2ED-501A-48ce-9863-49D468DDD5FC}": "LAU-68 - 7 MK1 (Practice)",
+    "{174C6E6D-0C3D-42ff-BCB3-0853CB371F5C}": "LAU-68 - 7 MK5 (HE)",
+    "{65396399-9F5C-4ec3-A7D2-5A8F4C1D90C4}": "LAU-68 - 7 MK61 (Practice)",
+    "{1F7136CB-8120-4e77-B97B-945FF01FB67C}": "LAU-68 - 7 WTU1B (Practice)",
+    "LAU_131x3_HYDRA_70_M151": "LAU-131*3 - 7 M151 (HE)",
+    "LAU_131x3_HYDRA_70_M156": "LAU-131*3 - 7 M156 (WP)",
+    "LAU_131x3_HYDRA_70_M257": "LAU-131*3 - 7 M257",
+    "LAU_131x3_HYDRA_70_M274": "LAU-131*3 - 7 M274",
+    "LAU_131x3_HYDRA_70_MK1": "LAU-131*3 - 7 MK1",
+    "LAU_131x3_HYDRA_70_MK5": "LAU-131*3 - 7 MK5",
+    "LAU_131x3_HYDRA_70_MK61": "LAU-131*3 - 7 MK61",
+    "LAU_131x3_HYDRA_70_WTU1B": "LAU-131*3 - 7 WTU1B",
+    "{64329ED9-B14C-4c0b-A923-A3C911DA1527}": "LAU-68*3 - 7 M151 (HE)",
+    "{C2593383-3CA8-4b18-B73D-0E750BCA1C85}": "LAU-68*3 - 7 M156 (WP)",
+    "{E6966004-A525-4f47-AF94-BCFEDF8FDBDA}": "LAU-68*3 - 7 M257",
+    "{4C044B08-886B-46c8-9B1F-AB05B3ED9C1D}": "LAU-68*3 - 7 M274",
+    "{443364AE-D557-488e-9499-45EDB3BA6730}": "LAU-68*3 - 7 MK1",
+    "{9BC82B3D-FE70-4910-B2B7-3E54EFE73262}": "LAU-68*3 - 7 MK5 (HE)",
+    "{C0FA251E-B645-4ce5-926B-F4BC20822F8B}": "LAU-68*3 - 7 MK61",
+    "{A1853B38-2160-4ffe-B7E9-9BF81E6C3D77}": "LAU-68*3 - 7 WTU1B",
+    "{LAU-131x3 - 7 AGR-20A}": "3 LAU-131 M151 - HE APKWS",
+    "{LAU-131x3 - 7 AGR-20 M282}": "3 LAU-131 M282 - MPP APKWS",
+    "LAU-105_1*AIM-9L_R": "LAU-105 AIM-9L",
+    "LAU-105_1*AIM-9M_R": "LAU-105 AIM-9M",
+    "{AV8BNA_AERO1D}": "AERO 1D 300gal Tank ",
+    "{AV8BNA_AERO1D_EMPTY}": "AERO 1D 300gal Tank (Empty)",
+    "{AIM-9M-ON-ADAPTER}": "LAU-7 AIM-9M",
+    "{M2KC_02_RPL541}": "RPL 541 2000L Tank ",
+    "{M2KC_02_RPL541_EMPTY}": "RPL 541 2000L Tank (Empty)",
+    "{M2KC_RPL_522}": "RPL 522 1300L Tank",
+    "{M2KC_RPL_522_EMPTY}": "RPL 522 1300L Tank (Empty)",
+    "{M2KC_08_RPL541}": "RPL 541 2000L Tank ",
+    "{M2KC_08_RPL541_EMPTY}": "RPL 541 2000L Tank (Empty)",
+    "M261_MK151": "M261 - 19 MK151 HE",
+    "M261_MK156": "M261 - 19 MK156 WP",
+    "XM158_M151": "XM158 - 7 M151 HE",
+    "XM158_M156": "XM158 - 7 M156 WP",
+    "XM158_M257": "XM158 - 7 M257",
+    "XM158_M274": "XM158 - 7 M274",
+    "XM158_MK1": "XM158 - 7 MK1 Practice",
+    "XM158_MK5": "XM158 - 7 MK5 HE",
+  }
 
   this.table = function() {
     return $(`
@@ -457,7 +541,16 @@ var Loadout = function(data, unit) {
 
       var pyl_name = elem.pyl;
       var store = elem.store || '';
-      var weight = store ? ntos(elem.weight || 0) : '';
+      var weight = '';
+
+      if (store) {
+        clsid = elem.clsid || '';
+        weight = ntos(elem.weight || 0);
+
+        if (clsid_renames[clsid]) {
+          store = clsid_renames[clsid];
+        }
+      }
 
       var other_itm  = other_col[n];
       var lbs_itm = lbs_col[n];
